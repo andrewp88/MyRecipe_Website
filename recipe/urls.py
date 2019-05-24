d2867e7bb0b85271849973f8d1de88baca611de1
Andrew88Notebook <andriypolulyakh@gmail.com>
2019-05-24 22:48:47 +0200

    Url redirectory fix

--- recipe/urls.py
+++ recipe/urls.py
@@ -5,7 +5,8 @@ urlpatterns = [
     path('newrecipe/',views.new_recipe_view,name='newrecipe'),
     path('myrecipe/',views.my_recipe_view,name='myrecipe'),
     path('savedrecipe/',views.saved_recipe_view,name='savedrecipe'),
-    path('home/',views.homepage,name='homepage')
+    path('home/',views.homepage,name='homepage'),
+    path('',views.homepage,name='hompage'),
     path('recipe/<int:my_id>/',views.detail_recipe_view,name="recipe"),
     path('recipe/<int:my_id>/delete/',views.delete_recipe_view,name="deleterecipe")
 
