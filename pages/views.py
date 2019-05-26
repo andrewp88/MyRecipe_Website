b45377f80e696247d6820e45febd73f184330122
valentina <valentinaprighel7@gmail.com>
2019-05-26 12:45:10 +0200

    small changes

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 
 def home_view(request,*args,**kwargs):
-    return render(request,"homepage.html",{})
+    return render(request,"recipe/homepage.html",{})
