5729910471d4e1136be33fa1ce2ba041e2aa9d09
valentina <valentinaprighel7@gmail.com>
2019-05-28 00:43:21 +0200

    stuff and fa fa-trash

--- recipe/forms.py
+++ recipe/forms.py
@@ -10,7 +10,7 @@ class SearchForm(forms.Form):
         max_length="50",
         label="",
         required=False,
-        widget=forms.TextInput(attrs={"placeholder": "Title, category, ingredients..", "class":"form-control form-control-sm mr-3"}))
+        widget=forms.TextInput(attrs={"placeholder": "Title, category, ingredients..", "class":"form-control form-control mr-3"}))
     class Meta:
         fields=[
             'searchInput'
